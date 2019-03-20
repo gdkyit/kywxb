0e6d78b5cbc886869f2990f4128bc2e88c34b272
Navin <navin.chung@gmail.com>
2019-03-20 18:20:02 +0800

    0320#01

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
   },
-  onShow: function () {
+  /* onShow: function () {
     let pages = getCurrentPages();
     if (pages.length > 0 && pages[pages.length - 1].route != 'pages/index/index') {
      // this.getSysMsg();
     }
-  },
+  }, */
   globalData: {},
   host: "https://apps.gdkyit.com/kxb-backend",
   uploadHost: 'https://apps.gdkyit.com/kxb-backend',
