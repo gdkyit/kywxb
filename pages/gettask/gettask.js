d6df3a7a3a0d561f6285e09624f4b807618bec4a
Navin <navin.chung@gmail.com>
2019-03-22 10:20:32 +0800

    0322

--- pages/gettask/gettask.js
+++ pages/gettask/gettask.js
@@ -1,9 +1,33 @@
 var app = getApp();
 Page({
-    data: {
-
-    },
-/*     onLoad: function (option) {
+  data: {
+    options: [
+      {
+        text: "价格",
+        type: "sort"
+      },
+      {
+        text: "面积",
+        type: "sort"
+      },
+      {
+        text: "完成日期",
+        type: "sort"
+      }
+    ],
+    tasks: [
+      { room: 1205, area: 30, price: 40 },
+      { room: 1206, area: 32, price: 42 },
+      { room: 1207, area: 32, price: 42 },
+      { room: 1208, area: 36, price: 45 },
+      { room: 1209, area: 40, price: 50 },
+      { room: 1210, area: 40, price: 50 },
+    ]
+  },
+  handleChange(e) {
+    console.log(e);
+  }
+  /*     onLoad: function (option) {
 
         wx.getStorage({ //获取token
             key: 'token',
@@ -24,7 +48,4 @@ Page({
             }
         })
     }, */
-    
-
-
 });
